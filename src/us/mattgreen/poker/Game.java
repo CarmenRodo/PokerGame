d3742e3bf9d7dbd424137c302fc01927e5be1769
Rodolfo <carmen.rodolfo2003@gmail.com>
2023-12-06 02:45:20 -0600

    Finished with all hand except for the Royal FLush

--- src/us/mattgreen/poker/Game.java
+++ src/us/mattgreen/poker/Game.java
@@ -1,26 +1,111 @@
 package us.mattgreen.poker;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * This is a utility class consisting of static methods to determine if a hand matches a specific
- * @author  Dr. Green
- * @version 1.0
- */
 public class Game {
-    /**
-     *
-     * @param hand An Array of Cards
-     * @return True if there are two of a kind and false if not
-     */
-    public static boolean twoOfAKind(Card[] hand) {
-        Arrays.sort(hand, (a, b) -> a.getFace().compareTo(b.getFace()));
-        int numberOFmatches = 0;
-        for(int i=0;i < hand.length-1; i++) {
-            if (hand[i].getFace().equals(hand[i+1].getFace())) return true;
-        }
-        return false;
+    private ArrayList<Card> hand;
+
+    public Game(ArrayList<Card> hand) {
+        this.hand = hand;
     }
 
+    public boolean isFullHouse() {
+        Map<Face, Integer> faceCounts = new HashMap<>();
+        for (Card card : hand) {
+            faceCounts.put(card.getFace(), faceCounts.getOrDefault(card.getFace(), 0) + 1);
+        }
+        return faceCounts.containsValue(3) && faceCounts.containsValue(2);
+    }
 
+    public boolean isFlush() {
+        Suit firstSuit = hand.get(0).getSuit();
+        for (Card card : hand) {
+            if (card.getSuit() != firstSuit) return false;
+        }
+        return true;
+    }
+
+    public boolean isThreeOfAKind() {
+        Map<Face, Integer> faceCounts = new HashMap<>();
+        for (Card card : hand) {
+            faceCounts.put(card.getFace(), faceCounts.getOrDefault(card.getFace(), 0) + 1);
+        }
+        return faceCounts.containsValue(3) && !faceCounts.containsValue(2);
+    }
+
+    public boolean isFourOfAKind() {
+        Map<Face, Integer> faceCounts = new HashMap<>();
+        for (Card card : hand) {
+            faceCounts.put(card.getFace(), faceCounts.getOrDefault(card.getFace(), 0) + 1);
+        }
+        return faceCounts.containsValue(4);
+    }
+    public boolean isStraight() {
+        // Check for a Straight: five consecutive cards of different suits.
+        ArrayList<Face> faces = new ArrayList<>();
+        for (Card card : hand) {
+            faces.add(card.getFace());
+        }
+        Collections.sort(faces);
+
+        for (int i = 1; i < faces.size(); i++) {
+            if (faces.get(i).ordinal() - faces.get(i - 1).ordinal() != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isTwoPair() {
+        // Check for Two Pair: two sets of pairs.
+        Map<Face, Integer> faceCounts = new HashMap<>();
+        for (Card card : hand) {
+            faceCounts.put(card.getFace(), faceCounts.getOrDefault(card.getFace(), 0) + 1);
+        }
+        int pairCount = 0;
+        for (Integer count : faceCounts.values()) {
+            if (count == 2) {
+                pairCount++;
+            }
+        }
+        return pairCount == 2;
+    }
+
+    public boolean isOnePair() {
+        // Check for One Pair: two cards of the same rank.
+        Map<Face, Integer> faceCounts = new HashMap<>();
+        for (Card card : hand) {
+            faceCounts.put(card.getFace(), faceCounts.getOrDefault(card.getFace(), 0) + 1);
+        }
+        return faceCounts.containsValue(2);
+    }
+
+    public boolean isHighCard() {
+        // Check for High Card: when no other poker hand is made, and you have the highest card.
+        Face highestFace = hand.get(0).getFace();
+        for (Card card : hand) {
+            if (card.getFace().ordinal() > highestFace.ordinal()) {
+                highestFace = card.getFace();
+            }
+        }
+        return true;
+    }
+    public boolean isRoyalFlush() {
+        // Check for a Royal Flush: A, K, Q, J, 10, all of the same suit.
+        Suit firstSuit = hand.get(0).getSuit();
+        for (Card card : hand) {
+            if (card.getSuit() != firstSuit || card.getFace() == Face.ACE || card.getFace() == Face.KING || card.getFace() == Face.QUEEN || card.getFace() == Face.JACK || card.getFace() == Face.TEN) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isStraightFlush() {
+        // Check for a Straight Flush: five consecutive cards of the same suit.
+        return isFlush() && isStraight();
+    }
 }
--- src/us/mattgreen/poker/test/GameTest.java
+++ src/us/mattgreen/poker/test/GameTest.java
@@ -5,20 +5,148 @@ import org.junit.Test;
 import us.mattgreen.poker.Card;
 import us.mattgreen.poker.Game;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertTrue;
 import static us.mattgreen.poker.Face.*;
 import static us.mattgreen.poker.Suit.*;
 
 public class GameTest {
-    private Card[] hand;
+    private Game game;
+    private ArrayList<Card> hand;
 
     @Before
     public void setUp() {
-        hand = new Card[]{new Card(JACK,CLUBS, 11), new Card(FIVE,CLUBS, 5), new Card(JACK,SPADES, 11)};
+        hand = new ArrayList<>();
+        // Example hand setup, can be modified for different tests
+        hand.add(new Card(THREE, CLUBS, 3));
+        hand.add(new Card(THREE, DIAMONDS, 3));
+        hand.add(new Card(THREE, HEARTS, 3));
+        hand.add(new Card(FIVE, SPADES, 5));
+        hand.add(new Card(FIVE, DIAMONDS, 5));
+        game = new Game(hand);
     }
 
     @Test
-    public void twoOfAKindTest() {
-        assertTrue("Two of a kind should have matched",Game.twoOfAKind(hand));
+    public void testFullHouse() {
+        assertTrue("Full house should have matched", game.isFullHouse());
     }
+
+    @Test
+    public void testFlush() {
+        // Set up a different hand for testing Flush
+        hand.clear();
+        hand.add(new Card(TEN, HEARTS, 10));
+        hand.add(new Card(JACK, HEARTS, 11));
+        hand.add(new Card(QUEEN, HEARTS, 12));
+        hand.add(new Card(KING, HEARTS, 13));
+        hand.add(new Card(ACE, HEARTS, 14));
+        game = new Game(hand);
+        assertTrue("Flush should have matched", game.isFlush());
+    }
+
+    @Test
+    public void testThreeOfAKind() {
+        // Set up a different hand for testing Three-of-a-Kind
+        hand.clear();
+        hand.add(new Card(TWO, SPADES, 2));
+        hand.add(new Card(TWO, HEARTS, 2));
+        hand.add(new Card(TWO, DIAMONDS, 2));
+        hand.add(new Card(FOUR, SPADES, 4));
+        hand.add(new Card(FIVE, CLUBS, 5));
+        game = new Game(hand);
+        assertTrue("Three-of-a-Kind should have matched", game.isThreeOfAKind());
+    }
+
+    @Test
+    public void testFourOfAKind() {
+        // Set up a different hand for testing Four-of-a-Kind
+        hand.clear();
+        hand.add(new Card(SEVEN, HEARTS, 7));
+        hand.add(new Card(SEVEN, DIAMONDS, 7));
+        hand.add(new Card(SEVEN, CLUBS, 7));
+        hand.add(new Card(SEVEN, SPADES, 7));
+        hand.add(new Card(FIVE, HEARTS, 5));
+        game = new Game(hand);
+        assertTrue("Four-of-a-Kind should have matched", game.isFourOfAKind());
+    }
+
+    @Test
+    public void testStraight() {
+        // Set up a Straight hand for testing
+        hand.clear();
+        hand.add(new Card(SIX, HEARTS, 6));
+        hand.add(new Card(SEVEN, DIAMONDS, 7));
+        hand.add(new Card(EIGHT, CLUBS, 8));
+        hand.add(new Card(NINE, SPADES, 9));
+        hand.add(new Card(TEN, HEARTS, 10));
+        game = new Game(hand);
+        assertTrue("Straight should have matched", game.isStraight());
+    }
+
+    @Test
+    public void testTwoPair() {
+        // Set up a Two Pair hand for testing
+        hand.clear();
+        hand.add(new Card(FIVE, HEARTS, 5));
+        hand.add(new Card(FIVE, DIAMONDS, 5));
+        hand.add(new Card(SEVEN, CLUBS, 7));
+        hand.add(new Card(SEVEN, SPADES, 7));
+        hand.add(new Card(JACK, HEARTS, 11));
+        game = new Game(hand);
+        assertTrue("Two Pair should have matched", game.isTwoPair());
+    }
+
+    @Test
+    public void testOnePair() {
+        // Set up a One Pair hand for testing
+        hand.clear();
+        hand.add(new Card(NINE, HEARTS, 9));
+        hand.add(new Card(NINE, DIAMONDS, 9));
+        hand.add(new Card(FOUR, CLUBS, 4));
+        hand.add(new Card(EIGHT, SPADES, 8));
+        hand.add(new Card(JACK, HEARTS, 11));
+        game = new Game(hand);
+        assertTrue("One Pair should have matched", game.isOnePair());
+    }
+
+    @Test
+    public void testHighCard() {
+        // Set up a High Card hand for testing
+        hand.clear();
+        hand.add(new Card(ACE, SPADES, 14));
+        hand.add(new Card(KING, HEARTS, 13));
+        hand.add(new Card(EIGHT, DIAMONDS, 8));
+        hand.add(new Card(QUEEN, CLUBS, 12));
+        hand.add(new Card(JACK, SPADES, 11));
+        game = new Game(hand);
+        assertTrue("High Card should have matched", game.isHighCard());
+    }
+    @Test
+    public void testStraightFlush() {
+        // Set up a Straight Flush hand for testing
+        hand.clear();
+        hand.add(new Card(SIX, HEARTS, 6));
+        hand.add(new Card(SEVEN, HEARTS, 7));
+        hand.add(new Card(EIGHT, HEARTS, 8));
+        hand.add(new Card(NINE, HEARTS, 9));
+        hand.add(new Card(TEN, HEARTS, 10));
+        game = new Game(hand);
+        assertTrue("Straight Flush should have matched", game.isStraightFlush());
+    }
+
+//    @Test
+//    public void testRoyalFlush() {
+//        // Set up a Royal Flush hand for testing
+//        hand.clear();
+//        hand.add(new Card(TEN, SPADES, 10));
+//        hand.add(new Card(JACK, SPADES, 11));
+//        hand.add(new Card(QUEEN, SPADES, 12));
+//        hand.add(new Card(KING, SPADES, 13));
+//        hand.add(new Card(ACE, SPADES, 14));
+//        game = new Game(hand);
+//        assertTrue("Royal Flush should have matched", game.isRoyalFlush());
+//    }
+
+
 }
